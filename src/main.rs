ea82dfe4967a7544a22443bb6790a93681a365a2
Grant Greer <viridiang93@gmail.com>
2023-05-30 12:09:30 -0700

    feat: add routes/handlers

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,86 @@
 use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
 use std::str;
-use std::net::{TcpStream, TcpListener};
 
-fn handle_client(mut stream: TcpStream) {
+struct Route {
+    path: &'static str,
+    method: &'static str,
+    handler: fn() -> &'static str, // A function pointer to the handler function
+}
+
+fn handle_root() -> &'static str {
+    "You requested the root path!"
+}
+
+fn handle_hello() -> &'static str {
+    "Hello, world!"
+}
+
+fn handle_client(mut stream: TcpStream, routes: &[Route]) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let request = str::from_utf8(&buffer).unwrap();
     let mut lines = request.lines();
 
-    // Get the first line of the request (the request line)
     let request_line = match lines.next() {
         Some(line) => line,
-        None => return, // If there's no data, just return
+        None => return,
     };
 
-    // Split the request line into components
     let components: Vec<&str> = request_line.split_whitespace().collect();
     if components.len() != 3 {
-        return; // If the request line doesn't have three components, it's not valid
+        return;
     }
 
     let method = components[0];
     let path = components[1];
 
-    if method == "GET" && path == "/" {
-        let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, world!";
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    } else {
-        let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
-}
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                handle_client(stream);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
+    let mut status_line = "";
+    let mut body = "";
+    for route in routes {
+        if method == route.method && path == route.path {
+            status_line = "HTTP/1.1 200 OK";
+            body = (route.handler)();
+            break;
         }
     }
 
-    drop(listener);
+    if status_line.is_empty() {
+        status_line = "HTTP/1.1 404 NOT FOUND";
+        body = "Not Found";
+    }
+
+    let response = format!(
+        "{}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        body.len(),
+        body
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind("localhost:7878").unwrap();
+
+    let routes = [
+        Route {
+            path: "/",
+            method: "GET",
+            handler: handle_root,
+        },
+        Route {
+            path: "/hello",
+            method: "GET",
+            handler: handle_hello,
+        },
+    ];
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_client(stream, &routes);
+    }
 }
