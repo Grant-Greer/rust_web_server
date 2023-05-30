0cf350e2e1def69c67c2a0ec4e51def73ca5bdc1
Grant Greer <viridiang93@gmail.com>
2023-05-30 04:22:33 -0700

    feat: add http req/res

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
-use std::alloc::{alloc, dealloc, Layout};
-use std::io::{Read, Write};
+use std::io::prelude::*;
+use std::str;
 use std::net::{TcpStream, TcpListener};
 
 fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-    match stream.read(&mut buffer) {
-        Ok(_) => {
-            println!("Received: {}", String::from_utf8_lossy(&buffer[..]));
-        }
-        Err(e) => {
-            println!("Failed to read from socket: {}", e);
-        }
+    let request = str::from_utf8(&buffer).unwrap();
+    let mut lines = request.lines();
+
+    // Get the first line of the request (the request line)
+    let request_line = match lines.next() {
+        Some(line) => line,
+        None => return, // If there's no data, just return
+    };
+
+    // Split the request line into components
+    let components: Vec<&str> = request_line.split_whitespace().collect();
+    if components.len() != 3 {
+        return; // If the request line doesn't have three components, it's not valid
+    }
+
+    let method = components[0];
+    let path = components[1];
+
+    if method == "GET" && path == "/" {
+        let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, world!";
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
 
