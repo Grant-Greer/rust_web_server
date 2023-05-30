6bd8a5d97ed206cd4932c8590c7c7d299cb17c23
Grant Greer <viridiang93@gmail.com>
2023-05-29 22:38:25 -0700

    feat: create tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::alloc::{alloc, dealloc, Layout};
+use std::io::{Read, Write};
+use std::net::{TcpStream, TcpListener};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    match stream.read(&mut buffer) {
+        Ok(_) => {
+            println!("Received: {}", String::from_utf8_lossy(&buffer[..]));
+        }
+        Err(e) => {
+            println!("Failed to read from socket: {}", e);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+
+    drop(listener);
 }
